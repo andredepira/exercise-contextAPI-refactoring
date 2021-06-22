52cd2c88b270d3c2ec8416f6ec32a5f84d38d515
André Arnoni <andre_arnoni@yahoo.com.br>
2021-06-22 19:41:20 -0300

    troca de branch

--- src/context/CarsProvider.js
+++ src/context/CarsProvider.js
@@ -8,16 +8,3 @@ const initialState = {
   },
 };
 
-function carReducer(state = initialState, action) {
-  switch (action.type) {
-    case MOVE_CAR:
-      return {
-        ...state,
-        cars: { ...state.cars, [action.car]: action.side }
-      };
-    default:
-      return state;
-  }
-}
-
-export default carReducer;
